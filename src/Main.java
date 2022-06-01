23695a173e513d43e4e8e88ce45cd26907eafe7b
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-06-01 13:45:06 +0300

    Лекция

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String phone = "7-960-415-75-37";
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7' ) {
+            throw new RuntimeException("Среди нас посторонний");
+            
+        }
+        System.out.println("phone = " + phone);
     }
 }
