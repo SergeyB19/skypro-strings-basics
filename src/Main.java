4e653b9219d08308668cd12d599164d0b90e63e3
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-06-01 13:59:56 +0300

    Лекция

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         String phone = "7-960-415-75-37";
+        phone = phone.replace(target, "-", replacement, "");
+        phone = phone.replace(target, "-", replacement, "");
+        phone = phone.replace(target, "+", replacement, "");
         if (phone.length() == 10) {
-            phone = '7' + phone;
+//            phone = '7' + phone;
         } else if (phone.length() > 11) {
             throw new RuntimeException("Телефон слишком длинный");
         } else if (phone.length() < 10) {
@@ -12,5 +15,11 @@ public class Main {
             
         }
         System.out.println("phone = " + phone);
+        String expectedPhone = "79604157537";
+        if (phone.equals(expectedPhone)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
     }
 }
